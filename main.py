f544406b27af55e8ab82f4d75a4564fad585f855
Serhii Honchar <serhii.honchar.java@gmail.com>
2024-11-05 00:20:20 +0100

    feat: make all button text bold for better user experience

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import font
 import ast
 
 # Create the main application window for the calculator
@@ -51,6 +52,8 @@ display = Entry(root, width=20, font=("Arial", 18), borderwidth=2,
                 relief="solid", justify='right')
 display.grid(row=0, column=0, columnspan=6, pady=(10, 10), sticky="EW")  # Position the display
 
+bold_font = font.Font(weight="bold")
+
 numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 counter = 0
 # Create buttons for numbers 1-9 in a 3x3 grid
@@ -59,13 +62,13 @@ for x in range(3):
         # Create a button for each number
         button_text = numbers[counter]
         button = Button(root, text=button_text, width=2, height=2, highlightthickness=0,
-                        command=lambda text=button_text: get_number(text))
+                        font=bold_font, command=lambda text=button_text: get_number(text))
         button.grid(row=x + 2, column=y)  # Position the button in the grid
         counter += 1
 
 # Create the button for number 0 and place it below the 1-9 buttons
 zero_button = Button(root, text=0, width=2, height=2, highlightthickness=0,
-                     command=lambda text=0: get_number(text))
+                     font=bold_font, command=lambda text=0: get_number(text))
 zero_button.grid(row=5, column=1)
 
 operations = ['+', '-', '*', '/', '*3.14', '%', '(', '**', ')', '**2']
@@ -78,17 +81,17 @@ for x in range(4):  # Loop over rows
             button_text = operations[count]  # Get the current operation
             # Create a button for the operation with a command to call get_operation on click
             button = Button(root, text=button_text, width=4, height=2, highlightthickness=0,
-                            command=lambda text=button_text: get_operation(text))
+                            font=bold_font, command=lambda text=button_text: get_operation(text))
             count += 1
             button.grid(row=x + 2, column=y + 3)  # Position the operation button
 
 # Create a button to clear the display with label 'AC' (All Clear)
 Button(root, text='AC', width=2, height=2, highlightthickness=0,
-       command=clear_all).grid(row=5, column=0)
+       font=bold_font, command=clear_all).grid(row=5, column=0)
 
 # Create a button to calculate the result with label '='
 Button(root, text='=', width=2, height=2, highlightthickness=0,
-       command=calculate).grid(row=5, column=2)
+       font=bold_font, command=calculate).grid(row=5, column=2)
 
 # Run the application
 root.mainloop()
